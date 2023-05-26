2be8b34b437e3633f92d1f8e0dfd541ed42f83fd
smo <126169960+samucatrem@users.noreply.github.com>
2023-05-26 11:09:03 -0300

    Update App.js

--- App.js
+++ App.js
@@ -10,6 +10,26 @@ export default function App() {
   );
 }
 
+//COMMMIT FINAL (SÓ MAIS UMA VEZ ESTOU ENSINANDO...)
+
+/*COMANDOS PARA FAZER O COMMIT
+
+  1)echo "# aula01" >> README.md
+  2)git init
+  3)git status -u
+  4)git add .
+  5)git status -u
+  6)git commit -m "first commit"
+  7)git config user.email "email do seu usuario no github"
+  8)gitconfig user.name "informação adicional"
+  9)git commit -m "first commit"
+  10)git status -u
+  11)git remote add origin "link do repositório do git hub"
+  12)git push -u origin master
+  13)git log
+
+*/
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
